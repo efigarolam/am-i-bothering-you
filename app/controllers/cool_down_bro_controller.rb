18b57a1b0ef18e1b0efa5da66e3cc1eb2f2e19d1
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-05-29 10:30:07 -0500

    First version of CoolDownController.

---
+++ app/controllers/cool_down_bro_controller.rb
@@ -0,0 +1,27 @@
+class CoolDownBroController < ApplicationController
+  before_action :find_bully
+
+  def index; end
+
+  def create
+    if @bully.present?
+      complaint = Complaint.create(complaint_params)
+
+      if complaint.persisted?
+        NotifyBullyMailer.mail(complaint).deliver
+      end
+    end
+  end
+
+  private
+
+  def complaint_params
+    params.require(:complaint).permit(:bully_id, :message)
+  end
+
+  def find_bully
+    @bully = Bully.find_by(username: params[:bully])
+  end
+
+end
+
